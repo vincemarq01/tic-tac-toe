dc46bbbd038a980daadef900b738183cd8cc19a7
vincemarq01 <vincemarqmontilla@gmail.com>
2024-06-06 17:56:31 +0800

    Adding winner functionality

--- src/components/TicTacToe.tsx
+++ src/components/TicTacToe.tsx
@@ -1,21 +1,56 @@
 
 import Board from './Board'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const TicTacToe = () => {
-    const [value, setValue] = useState("")
+    const [value, setValue] = useState<string[]>([])
+    const [player, setPlayer] = useState("X")
+    const [winner, setWinner] = useState<string>('');
+    
+    const checkwinner=()=>{
+        const winnercode = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6],
+        ]
+
+        for(let i=0; i<winnercode.length; i++){
+            const [a,b,c] = winnercode[i];
+            console.log(a,b,c);
+            if(value[a] !== null && value[a] === value[b] && value[a] === value[c]){
+            return value[a]
+            }
+     }
+            return null;
+    }
+ 
+
     const handleClick = (index:number)=>{
-        console.log(index, "index")
-        if (index === 0){
-           setValue("X");
+        const tilesnumber = [...value];
+        tilesnumber[index] = player;
+        setValue(tilesnumber);
+        setPlayer(player === "X"?"O":"X")
+        console.log( tilesnumber[index] , index);
+    }
+
+    useEffect(()=>{
+        const newWinner = checkwinner();
+        if(newWinner){
+            setWinner(newWinner)
         }
     
-    }
-    console.log(setValue, "test1");
+    },[value])
+
+  
   return (
     <div>
       <Board onClick={handleClick} value={value}/>
-    
+     {winner ? <h1 className='text-9xl'>winner</h1> :<h1>r</h1>}
     </div>
   
   )
--- src/components/Tiles.tsx
+++ src/components/Tiles.tsx
@@ -1,13 +1,13 @@
 
-type tiles ={
+type tiles = {
     onClick: ()=> void,
     value:string
+    
 }
 const Tiles = ({onClick,value}:tiles) => {
   return (
     <div>
-    <div className='gridBox text-9xl text-center' onClick={onClick}>{value}</div>
-     
+    <div className='gridBox text-9xl text-center'  onClick={onClick}>{value} </div>
     </div>
   )
 }
